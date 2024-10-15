bc77f92623001db710ccd16cfd1ae9502dbadc0d
Alexey <remaxux@proton.me>
2024-10-15 11:50:53 +0300

    Update all files

---
+++ TestAppStore/src/main/java/App.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        DbConnectionImpl connection = new DbConnectionImpl();
+
+        connection.insert(new User("user1", "123456780"));
+        connection.insert(new User("user2", "qwertyqwerty"));
+        connection.insert(new User("user3", "0987654321"));
+        connection.select();
+
+        // Изменить символы в английский транслит
+        String line = "АбВгААВВ";
+
+        for (int i = 0; i < line.length(); i++) {
+            char ch = line.charAt(i);
+
+            if (!map.containsKey(ch)) {
+                System.out.print(ch);
+                continue;
+            }
+            System.out.print(map.get(ch));
+        }
+
+        // Приветствие, в зависимости от времени
+        // В метод превратить
+        java.time.LocalTime time = java.time.LocalTime.now();
+        if (time.getHour() > 0 && time.getHour() < 6) {
+            System.out.println("Доброй ночи, ");
+        } else if (time.getHour() >= 6 && time.getHour() < 12) {
+            System.out.println("Доброе утро, ");
+        } else if (time.getHour() >= 12 && time.getHour() < 18) {
+            System.out.println("Добрый день, ");
+        } else {
+            System.out.println("Добрый вечер, ");
+        }
+    }
+    private static Map<Character, String> map = new HashMap<Character, String>() {
+        {
+            put('а', "a");
+            put('б', "b");
+            put('в', "v");
+            put('г', "g");
+            put('д', "d");
+            put('е', "e");
+            put('ж', "zh");
+            put('з', "z");
+            put('и', "i");
+            put('й', "j");
+            put('к', "k");
+            put('л', "l");
+            put('м', "m");
+            put('н', "n");
+            put('о', "o");
+            put('п', "p");
+            put('р', "r");
+            put('с', "s");
+            put('т', "t");
+            put('у', "u");
+            put('ф', "f");
+            put('х', "x");
+            put('ц', "c");
+            put('ч', "ch");
+            put('ш', "sh");
+            put('щ', "shh");
+            put('ы', "y");
+            put('э', "e");
+            put('ю', "yu");
+            put('я', "ya");
+        }
+    };
+}
